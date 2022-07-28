d122ff9d7ffbd3c099112296bbba88b830baca5e
Arun Maiya <arun@maiya.net>
2022-07-28 19:59:53 -0400

    added install commenter

---
+++ install-commenter.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+curl -fLo ~/.vim/plugin/NERD_Commenter.vim --create-dirs \
+  https://raw.githubusercontent.com/amaiya/dotfiles/main/.vim/plugin/NERD_commenter.vim
+curl -fLo ~/.vim/doc/NERD_Commenter.txt --create-dirs \
+  https://raw.githubusercontent.com/amaiya/dotfiles/main/.vim/doc/NERD_commenter.txt
